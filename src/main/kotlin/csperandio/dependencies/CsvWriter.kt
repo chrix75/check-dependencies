7819c262f34514c464df9a80f591e973fa061a2b
Christian Sperandio <csperandio@amabis.fr>
2021-05-11 11:43:33 +0200

    add main app

---
+++ src/main/kotlin/csperandio/dependencies/CsvWriter.kt
@@ -0,0 +1,16 @@
+package csperandio.dependencies
+
+import csperandio.dependencies.dependencies.Dependency
+import java.io.File
+
+class CsvWriter(val result: File) {
+    fun write(allDates: Set<Pair<Dependency, String>>) {
+        addHeader()
+        allDates.forEach { result.appendText("${it.first.group}:${it.first.artefact},${it.first.version},${it.second}\n") }
+    }
+
+    private fun addHeader() {
+        result.writeText("dependency,version,date\n")
+
+    }
+}
