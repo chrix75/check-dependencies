666b0f27e20606f9f4c86060cdb0e61d8abcf4df
Christian Sperandio <christian.sperandio@gmail.com>
2021-05-12 07:11:16 +0200

    add idiomatic method

--- src/main/kotlin/csperandio/dependencies/VersionChecker.kt
+++ src/main/kotlin/csperandio/dependencies/VersionChecker.kt
@@ -3,6 +3,7 @@ package csperandio.dependencies
 import csperandio.dependencies.dependencies.DependenciesSearch
 import csperandio.dependencies.dependencies.Dependency
 import csperandio.dependencies.repo.MavenRepo
+import java.io.File
 
 class VersionChecker(private vararg val repos: MavenRepo) {
     fun date(dependency: Dependency): String? {
@@ -16,4 +17,6 @@ class VersionChecker(private vararg val repos: MavenRepo) {
         val map = dependencies.associateWith { date(it) }
         return map.filterValues { it != null } as Map<Dependency, String>
     }
+
+    fun dates(pom: File) = dates(pom.readText())
 }
