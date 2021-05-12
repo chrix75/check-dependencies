bb01938d7ade501612ef35849ff3a105481d9bb2
Christian Sperandio <christian.sperandio@gmail.com>
2021-05-12 19:44:44 +0200

    manage plugins version

--- src/main/kotlin/csperandio/dependencies/dependencies/DependenciesSearch.kt
+++ src/main/kotlin/csperandio/dependencies/dependencies/DependenciesSearch.kt
@@ -1,5 +1,6 @@
 package csperandio.dependencies.dependencies
 
+import org.apache.maven.model.Model
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader
 
 class DependenciesSearch {
@@ -8,9 +9,17 @@ class DependenciesSearch {
         val reader = MavenXpp3Reader()
         val model = reader.read(pom.reader())
 
-        return model.dependencies
-            .filter { it.groupId != null && it.artifactId != null && it.version != null }
-            .map {
+        val allDependencies = mutableListOf<Dependency>()
+        with(allDependencies) {
+            addAll(mavenDependencies(model))
+            addAll(mavenPluginDependencies(model))
+        }
+        return allDependencies
+    }
+
+    private fun mavenPluginDependencies(model: Model): List<Dependency> {
+        return model.build?.plugins?.filter { it.groupId != null && it.artifactId != null && it.version != null }
+            ?.mapNotNull {
                 if (it.version.startsWith("\${")) {
                     val propName = it.version.substring(2, it.version.length - 1)
                     val realVersion = model.properties[propName] as String?
@@ -19,7 +28,21 @@ class DependenciesSearch {
                     Dependency(it.groupId, it.artifactId, it.version)
                 }
             }
-            .filterNotNull()
+            ?: emptyList()
+    }
+
+    private fun mavenDependencies(model: Model): List<Dependency> {
+        return model.dependencies
+            .filter { it.groupId != null && it.artifactId != null && it.version != null }
+            .mapNotNull {
+                if (it.version.startsWith("\${")) {
+                    val propName = it.version.substring(2, it.version.length - 1)
+                    val realVersion = model.properties[propName] as String?
+                    if (realVersion == null) null else Dependency(it.groupId, it.artifactId, realVersion)
+                } else {
+                    Dependency(it.groupId, it.artifactId, it.version)
+                }
+            }
     }
 
 }
--- src/test/kotlin/csperandio/dependencies/tests/unit/DependenciesSearchTest.kt
+++ src/test/kotlin/csperandio/dependencies/tests/unit/DependenciesSearchTest.kt
@@ -92,4 +92,58 @@ class DependenciesSearchTest {
         )
         assertEquals(expected, dependencies)
     }
+
+    @Test
+    internal fun managePlugin() {
+        val pomWithProperties =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                    "<project xmlns=\"http://maven.apache.org/POM/4.0.0\"\n" +
+                    "         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+                    "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
+                    "    <modelVersion>4.0.0</modelVersion>\n" +
+                    "\n" +
+                    "    <groupId>deduplication.company</groupId>\n" +
+                    "    <artifactId>comparison</artifactId>\n" +
+                    "    <version>1.0-SNAPSHOT</version>\n" +
+                    "\n" +
+                    "    <dependencies>\n" +
+                    "        <dependency>\n" +
+                    "            <groupId>org.projectlombok</groupId>\n" +
+                    "            <artifactId>lombok</artifactId>\n" +
+                    "            <version>1.16.20</version>\n" +
+                    "        </dependency>\n" +
+                    "        <dependency>\n" +
+                    "            <groupId>io.cucumber</groupId>\n" +
+                    "            <artifactId>cucumber-junit</artifactId>\n" +
+                    "            <version>2.3.1</version>\n" +
+                    "            <scope>test</scope>\n" +
+                    "        </dependency>\n" +
+                    "    </dependencies>\n" +
+                    "\n" +
+                    "    <build>\n" +
+                    "        <plugins>\n" +
+                    "            <plugin>\n" +
+                    "                <groupId>org.apache.maven.plugins</groupId>\n" +
+                    "                <artifactId>maven-compiler-plugin</artifactId>\n" +
+                    "                <version>3.7.0</version>\n" +
+                    "                <configuration>\n" +
+                    "                    <source>1.8</source>\n" +
+                    "                    <target>1.8</target>\n" +
+                    "                </configuration>\n" +
+                    "            </plugin>\n" +
+                    "        </plugins>\n" +
+                    "    </build>\n" +
+                    "\n" +
+                    "</project>"
+
+        val search = DependenciesSearch()
+        val dependencies = search.dependencies(pomWithProperties)
+
+        val expected = listOf(
+            Dependency("org.projectlombok", "lombok", "1.16.20"),
+            Dependency("io.cucumber", "cucumber-junit", "2.3.1"),
+            Dependency("org.apache.maven.plugins", "maven-compiler-plugin", "3.7.0"),
+        )
+        assertEquals(expected, dependencies)
+    }
 }
