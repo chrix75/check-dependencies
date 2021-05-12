abb8d9c2b20d6bbea8597796d9443c608770646c
Christian Sperandio <csperandio@amabis.fr>
2021-05-12 15:56:09 +0200

    manage missing Maven dependency information

--- src/main/kotlin/csperandio/dependencies/dependencies/DependenciesSearch.kt
+++ src/main/kotlin/csperandio/dependencies/dependencies/DependenciesSearch.kt
@@ -7,7 +7,10 @@ class DependenciesSearch {
     fun dependencies(pom: String): List<Dependency> {
         val reader = MavenXpp3Reader()
         val model = reader.read(pom.reader())
-        return model.dependencies.map { Dependency(it.groupId, it.artifactId, it.version) }
+        return model.dependencies
+            .filter { it.groupId != null && it.artifactId != null && it.version != null }
             .filter { !it.version.startsWith("\${") }
+            .map { Dependency(it.groupId, it.artifactId, it.version) }
+
     }
 }
